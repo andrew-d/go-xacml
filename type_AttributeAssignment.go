e09d10cc94accd35efac92a05bc081b9ae8ab79f
Andrew Dunham <andrew@du.nham.ca>
2016-08-02 00:29:34 -0700

    Fix AttributeAssignment, add test

--- type_AttributeAssignment.go
+++ type_AttributeAssignment.go
@@ -11,22 +11,23 @@ import "encoding/xml"
 // further specified by XACML. See Section 7.18.  Section 4.2.4.3 provides a
 // number of examples of arguments included in obligation.expressions.
 type AttributeAssignment struct {
+	AttributeValue
 	XMLName xml.Name `xml:"AttributeAssignment"`
 
 	// The attribute identifier
-	AttributeId string // TODO: required
+	AttributeId string `xml:",attr"` // TODO: required
 
 	// An optional category of the attribute. If this attribute is missing, the
 	// attribute has no category. The PEP SHALL interpret the significance and
 	// meaning of any Category attribute. Non-normative note: an expected use
 	// of the category is to disambiguate attributes which are relayed from the
 	// request.
-	Category string
+	Category string `xml:",attr"`
 
 	// An optional issuer of the attribute. If this attribute is missing, the
 	// attribute has no issuer. The PEP SHALL interpret the significance and
 	// meaning of any Issuer attribute. Non-normative note: an expected use of
 	// the issuer is to disambiguate attributes which are relayed from the
 	// request.
-	Issuer string
+	Issuer string `xml:",attr"`
 }
--- type_AttributeAssignment_test.go
+++ type_AttributeAssignment_test.go
@@ -8,11 +8,23 @@ import (
 )
 
 func Test_AttributeAssignment(t *testing.T) {
-	input := `<AttributeAssignment></AttributeAssignment>`
+	input := `
+	<AttributeAssignment
+		AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:assignment1"
+		DataType="http://www.w3.org/2001/XMLSchema#string"
+		Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource"
+		Issuer="an-issuer">assignment1</AttributeAssignment>
+	`
 
-    dest := &AttributeAssignment{}
+	dest := &AttributeAssignment{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element AttributeAssignment")
 
 	// Insert specific tests here
+	assert.Equal(t, "urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:assignment1", dest.AttributeId)
+	assert.Equal(t, "http://www.w3.org/2001/XMLSchema#string", dest.DataType)
+	assert.Equal(t, "urn:oasis:names:tc:xacml:3.0:attribute-category:resource", dest.Category)
+	assert.Equal(t, "an-issuer", dest.Issuer)
+
+	assert.Equal(t, "assignment1", dest.Value)
 }
