7c13e78afb553fc719836d1164c0fb36451f5735
Andrew Dunham <andrew@du.nham.ca>
2016-08-02 00:56:37 -0700

    Implement <Advice>

--- type_Advice.go
+++ type_Advice.go
@@ -2,9 +2,17 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.35: The <Advice> element SHALL contain an identifier for the advice and a
+// set of attributes that form arguments of the supplemental information
+// defined by the advice.
 type Advice struct {
 	XMLName xml.Name `xml:"Advice"`
 
-	// Insert fields here
+	// Advice identifier.  The value of the advice identifier MAY be
+	// interpreted by the PEP.
+	AdviceId string `xml:",attr"` // TODO: required
+
+	// Advice arguments assignment.  The values of the advice arguments MAY be
+	// interpreted by the PEP.
+	Assignments []AttributeAssignment `xml:"AttributeAssignment"`
 }
--- type_Advice_test.go
+++ type_Advice_test.go
@@ -8,11 +8,33 @@ import (
 )
 
 func Test_Advice(t *testing.T) {
-	input := `<Advice></Advice>`
+	input := `
+	<Advice
+		AdviceId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:Advice-1">
+		<AttributeAssignment
+			AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:assignment1"
+			DataType="http://www.w3.org/2001/XMLSchema#string">assignment1</AttributeAssignment>
+		<AttributeAssignment
+			AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicSingleValue"
+			DataType="http://www.w3.org/2001/XMLSchema#string">J. Hibbert</AttributeAssignment>
+	</Advice>`
 
-    dest := &Advice{}
+	dest := &Advice{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Advice")
 
 	// Insert specific tests here
+	assert.Equal(t, "urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:Advice-1", dest.AdviceId)
+
+	if assert.Len(t, dest.Assignments, 2) {
+		a1 := dest.Assignments[0]
+		assert.Equal(t, "urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:assignment1", a1.AttributeId)
+		assert.Equal(t, "http://www.w3.org/2001/XMLSchema#string", a1.DataType)
+		assert.Equal(t, "assignment1", a1.Value)
+
+		a2 := dest.Assignments[1]
+		assert.Equal(t, "urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicSingleValue", a2.AttributeId)
+		assert.Equal(t, "http://www.w3.org/2001/XMLSchema#string", a2.DataType)
+		assert.Equal(t, "J. Hibbert", a2.Value)
+	}
 }
