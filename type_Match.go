b17e5369a8345d379074586bf3ac1fc550f156a0
Andrew Dunham <andrew@du.nham.ca>
2016-08-07 15:39:32 -0700

    Implement <Match>

--- type_Match.go
+++ type_Match.go
@@ -2,9 +2,26 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.9: The <Match> element SHALL identify a set of entities by matching
+// attribute values in an <Attributes> element of the request context with the
+// embedded attribute value.
 type Match struct {
 	XMLName xml.Name `xml:"Match"`
 
-	// Insert fields here
+	// Specifies a matching function.  The value of this attribute MUST be of
+	// type xs:anyURI with legal values documented in Section 7.6.
+	MatchId string `xml:",attr"` // TODO: required
+
+	// Embedded attribute value.
+	AttributeValue AttributeValue // TODO: required
+
+	// TODO: one of the following two is required
+
+	// MAY be used to identify one or more attribute values in an <Attributes>
+	// element of the request context.
+	AttributeDesignator *AttributeDesignator
+
+	// MAY be used to identify one or more attribute values in a <Content>
+	// element of the request context.
+	AttributeSelector *AttributeSelector
 }
--- type_Match_test.go
+++ type_Match_test.go
@@ -8,11 +8,26 @@ import (
 )
 
 func Test_Match(t *testing.T) {
-	input := `<Match></Match>`
+	input := `
+	<Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+		<AttributeValue
+			DataType="http://www.w3.org/2001/XMLSchema#string">read</AttributeValue>
+		<AttributeDesignator
+			AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id"
+			Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action"
+			DataType="http://www.w3.org/2001/XMLSchema#string"
+			MustBePresent="false"/>
+	</Match>`
 
-    dest := &Match{}
+	dest := &Match{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Match")
 
-	// Insert specific tests here
+	assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:function:string-equal", dest.MatchId)
+	assert.Equal(t, "read", dest.AttributeValue.Value)
+
+	assert.Nil(t, dest.AttributeSelector)
+	if assert.NotNil(t, dest.AttributeDesignator) {
+		assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:action:action-id", dest.AttributeDesignator.AttributeId)
+	}
 }
