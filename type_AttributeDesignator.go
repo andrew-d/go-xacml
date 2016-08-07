24b5f2d0880d43c981a01e353ce4336c3a3526ed
Andrew Dunham <andrew@du.nham.ca>
2016-08-07 15:35:55 -0700

    Implement AttributeDesignator

--- type_AttributeDesignator.go
+++ type_AttributeDesignator.go
@@ -2,9 +2,43 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.29: The <AttributeDesignator> element retrieves a bag of values for a
+// named attribute from the request context.  A named attribute SHALL be
+// considered present if there is at least one attribute that matches the
+// criteria set out below.
+//
+// The <AttributeDesignator> element SHALL return a bag containing all the
+// attribute values that are matched by the named attribute.  In the event that
+// no matching attribute is present in the context, the MustBePresent attribute
+// governs whether this element returns an empty bag or “Indeterminate”.  See
+// Section 7.3.5.
+//
+// The <AttributeDesignator> MAY appear in the <Match> element and MAY be
+// passed to the <Apply> element as an argument.
+//
+// The <AttributeDesignator> element is of the AttributeDesignatorType complex
+// type.
 type AttributeDesignator struct {
 	XMLName xml.Name `xml:"AttributeDesignator"`
 
-	// Insert fields here
+	// This attribute SHALL specify the AttributeId with which to match the
+	// attribute.
+	AttributeId string `xml:",attr"` // TODO: required
+
+	// This attribute SHALL specify the Category with which to match the
+	// attribute.
+	Category string `xml:",attr"` // TODO: required
+
+	// The bag returned by the <AttributeDesignator> element SHALL contain
+	// values of this data-type.
+	DataType string `xml:",attr"` // TODO: required
+
+	// This attribute, if supplied, SHALL specify the Issuer with which to
+	// match the attribute.
+	Issuer string `xml:",attr"`
+
+	// This attribute governs whether the element returns “Indeterminate” or an
+	// empty bag in the event the named attribute is absent from the request
+	// context.  See Section 7.3.5.  Also see Sections 7.19.2 and 7.19.3.
+	MustBePresent bool `xml:",attr"` // TODO: required
 }
--- type_AttributeDesignator_test.go
+++ type_AttributeDesignator_test.go
@@ -8,11 +8,22 @@ import (
 )
 
 func Test_AttributeDesignator(t *testing.T) {
-	input := `<AttributeDesignator></AttributeDesignator>`
+	input := `
+	<AttributeDesignator
+		AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id"
+		Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action"
+		DataType="http://www.w3.org/2001/XMLSchema#string"
+		Issuer="an-issuer"
+		MustBePresent="false"
+		/>`
 
-    dest := &AttributeDesignator{}
+	dest := &AttributeDesignator{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element AttributeDesignator")
 
-	// Insert specific tests here
+	assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:action:action-id", dest.AttributeId)
+	assert.Equal(t, "urn:oasis:names:tc:xacml:3.0:attribute-category:action", dest.Category)
+	assert.Equal(t, "http://www.w3.org/2001/XMLSchema#string", dest.DataType)
+	assert.Equal(t, "an-issuer", dest.Issuer)
+	assert.Equal(t, false, dest.MustBePresent)
 }
