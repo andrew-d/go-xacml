0ca81d635a9ac1002eabd66e6ebeb3aa034f481f
Andrew Dunham <adunham@stripe.com>
2016-08-08 20:34:29 -0700

    Implement <AllOf>

--- type_AllOf.go
+++ type_AllOf.go
@@ -2,9 +2,24 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.8: The <AllOf> element SHALL contain a conjunctive sequence of <Match>
+// elements.
 type AllOf struct {
 	XMLName xml.Name `xml:"AllOf"`
 
-	// Insert fields here
+	// A conjunctive sequence of individual matches of the attributes in
+	// the request context and the embedded attribute values.  See Section
+	// 5.9.
+	Matches []Match `xml:"Match"`
+}
+
+func (a AllOf) Validate(errs *Errors) {
+	if len(a.Matches) < 1 {
+		errs.Addf("Matches not given")
+		return
+	}
+
+	for i, match := range a.Matches {
+		match.Validate(errs.SubN("Matches", i))
+	}
 }
--- type_AllOf_test.go
+++ type_AllOf_test.go
@@ -8,11 +8,37 @@ import (
 )
 
 func Test_AllOf(t *testing.T) {
-	input := `<AllOf></AllOf>`
+	input := `
+	<AllOf>
+		<Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+			<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">John Doe</AttributeValue>
+			<AttributeDesignator
+				AttributeId="urn:oasis:names:tc:xacml:1.0:subject:subject-id"
+				Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject"
+				DataType="http://www.w3.org/2001/XMLSchema#string"
+				MustBePresent="false"/>
+		</Match>
+		<Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+			<AttributeValue
+				DataType="http://www.w3.org/2001/XMLSchema#anyURI">FakeLocation</AttributeValue>
+			<AttributeDesignator
+				AttributeId="urn:oasis:names:tc:xacml:1.0:resource:resource-id"
+				Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource"
+				DataType="http://www.w3.org/2001/XMLSchema#anyURI"
+				MustBePresent="false"/>
 
-    dest := &AllOf{}
+		</Match>
+	</AllOf>`
+
+	dest := &AllOf{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element AllOf")
 
-	// Insert specific tests here
+	if assert.Len(t, dest.Matches, 2) {
+		assert.Equal(t, "John Doe", dest.Matches[0].AttributeValue.Value)
+		assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:subject:subject-id", dest.Matches[0].AttributeDesignator.AttributeId)
+
+		assert.Equal(t, "FakeLocation", dest.Matches[1].AttributeValue.Value)
+		assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:resource:resource-id", dest.Matches[1].AttributeDesignator.AttributeId)
+	}
 }
