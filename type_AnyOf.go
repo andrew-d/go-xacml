fc703eb4b72b86b18dd377ada797fe309435a13b
Andrew Dunham <adunham@stripe.com>
2016-08-08 20:38:24 -0700

    Implement <AnyOf>

--- type_AnyOf.go
+++ type_AnyOf.go
@@ -2,9 +2,22 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.7: The <AnyOf> element SHALL contain a disjunctive sequence of <AllOf>
+// elements.
 type AnyOf struct {
 	XMLName xml.Name `xml:"AnyOf"`
 
-	// Insert fields here
+	// See Section 5.8.
+	AllOf []AllOf `xml:"AllOf"`
+}
+
+func (a AnyOf) Validate(errs *Errors) {
+	if len(a.AllOf) < 1 {
+		errs.Addf("AllOf not given")
+		return
+	}
+
+	for i, all := range a.AllOf {
+		all.Validate(errs.SubN("AllOf", i))
+	}
 }
--- type_AnyOf_test.go
+++ type_AnyOf_test.go
@@ -8,11 +8,39 @@ import (
 )
 
 func Test_AnyOf(t *testing.T) {
-	input := `<AnyOf></AnyOf>`
+	input := `
+	<AnyOf>
+		<AllOf>
+			<Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+				<AttributeValue
+					DataType="http://www.w3.org/2001/XMLSchema#string">read</AttributeValue>
+				<AttributeDesignator
+					AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id"
+					Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action"
+					DataType="http://www.w3.org/2001/XMLSchema#string"
+					MustBePresent="false"/>
+			</Match>
+		</AllOf>
+		<AllOf>
+			<Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+				<AttributeValue
+					DataType="http://www.w3.org/2001/XMLSchema#string">write</AttributeValue>
+				<AttributeDesignator
+					AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id"
+					Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action"
+					DataType="http://www.w3.org/2001/XMLSchema#string"
+					MustBePresent="false"/>
+			</Match>
+		</AllOf>
+	</AnyOf>`
 
-    dest := &AnyOf{}
+	dest := &AnyOf{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element AnyOf")
 
-	// Insert specific tests here
+	if assert.Len(t, dest.AllOf, 2) {
+		assert.Equal(t, "read", dest.AllOf[0].Matches[0].AttributeValue.Value)
+
+		assert.Equal(t, "write", dest.AllOf[1].Matches[0].AttributeValue.Value)
+	}
 }
