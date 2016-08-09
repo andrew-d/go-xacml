14bc2db44f072777c0f405869c19fce0afec1f5d
Andrew Dunham <adunham@stripe.com>
2016-08-09 16:42:36 -0700

    Implement <XPathVersion>

--- type_XPathVersion.go
+++ type_XPathVersion.go
@@ -1,10 +1,38 @@
 package xacml
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"strings"
+)
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.5: The <XPathVersion> element SHALL specify the version of the XPath
+// specification to be used by <AttributeSelector> elements and XPath-based
+// functions in the policy set or policy.
 type XPathVersion struct {
 	XMLName xml.Name `xml:"XPathVersion"`
+	URI     string   `xml:",chardata"`
+}
 
-	// Insert fields here
+func (v XPathVersion) Validate(errs *Errors) {
+	if v.URI == "" {
+		errs.Addf("XPathVersion not given")
+		return
+	}
+
+	validURIs := []string{
+		"http://www.w3.org/TR/1999/REC-xpath-19991116",
+		"http://www.w3.org/TR/2007/REC-xpath20-20070123",
+	}
+
+	valid := false
+	for _, uri := range validURIs {
+		if strings.ToLower(uri) == strings.ToLower(v.URI) {
+			valid = true
+			break
+		}
+	}
+
+	if !valid {
+		errs.Addf("Unknown URI for XPathVersion: %s", v.URI)
+	}
 }
--- type_XPathVersion_test.go
+++ type_XPathVersion_test.go
@@ -8,11 +8,16 @@ import (
 )
 
 func Test_XPathVersion(t *testing.T) {
-	input := `<XPathVersion></XPathVersion>`
+	input := `<XPathVersion>http://www.w3.org/TR/1999/Rec-xpath-19991116</XPathVersion>`
 
 	dest := &XPathVersion{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element XPathVersion")
 
-	// Insert specific tests here
+	assert.Equal(t, "http://www.w3.org/TR/1999/Rec-xpath-19991116", dest.URI)
+
+	errs := NewErrors("XPathVersion")
+	dest.Validate(errs)
+
+	assert.Equal(t, 0, errs.NumErrors(), "Validate() returned errors:\n%s", errs.Summary())
 }
