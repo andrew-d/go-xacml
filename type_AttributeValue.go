8fe74f0fa7adc786875b4f09d6edea85043259c9
Andrew Dunham <adunham@stripe.com>
2016-08-08 20:07:37 -0700

    Refactor how we handle <Expression>

--- type_AttributeValue.go
+++ type_AttributeValue.go
@@ -4,18 +4,17 @@ import "encoding/xml"
 
 // 5.31: The <AttributeValue> element SHALL contain a literal attribute value.
 type AttributeValue struct {
+	Expression
 	XMLName xml.Name `xml:"AttributeValue"`
 
 	// The data-type of the attribute value.
 	DataType string `xml:",attr"`
-
-	// The value
-	// TODO: Do we need to use "innerxml", or can we actually use "chardata"?
-	Value string `xml:",chardata"`
 }
 
 func (a AttributeValue) Validate(errs *Errors) {
 	if a.DataType == "" {
 		errs.Addf("DataType not given")
 	}
+
+	a.Expression.Validate(errs)
 }
--- type_Expression.go
+++ type_Expression.go
@@ -2,9 +2,17 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.25: The <Expression> element is not used directly in a policy.  The
+// <Expression> element signifies that an element that extends the
+// ExpressionType and is a member of the <Expression> element substitution
+// group SHALL appear in its place.
 type Expression struct {
 	XMLName xml.Name `xml:"Expression"`
 
-	// Insert fields here
+	// The value
+	// TODO: Do we need to use "innerxml", or can we actually use "chardata"?
+	Value string `xml:",chardata"`
+}
+
+func (e Expression) Validate(errs *Errors) {
 }
