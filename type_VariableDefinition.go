edf9b6b7568e5bc827965a189309371c9f86d217
Andrew Dunham <adunham@stripe.com>
2016-08-10 16:57:18 -0700

    Implement <VariableDefinition>

--- type_VariableDefinition.go
+++ type_VariableDefinition.go
@@ -2,12 +2,55 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.23: The <VariableDefinition> element SHALL be used to define a value that
+// can be referenced by a <VariableReference> element.  The name supplied for
+// its VariableId attribute SHALL NOT occur in the VariableId attribute of any
+// other <VariableDefinition> element within the encompassing policy.  The
+// <VariableDefinition> element MAY contain undefined <VariableReference>
+// elements, but if it does, a corresponding <VariableDefinition> element MUST
+// be defined later in the encompassing policy.  <VariableDefinition> elements
+// MAY be grouped together or MAY be placed close to the reference in the
+// encompassing policy.  There MAY be zero or more references to each
+// <VariableDefinition> element.
 type VariableDefinition struct {
 	XMLName xml.Name `xml:"VariableDefinition"`
 
-	// Insert fields here
+	// The name of the variable definition.
+	VariableId string `xml:",attr"`
+
+	// Any element of ExpressionType complex type.
+	Expression ExpressionType
+}
+
+func (v *VariableDefinition) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
+	for _, attr := range start.Attr {
+		switch attr.Name.Local {
+		case "VariableId":
+			v.VariableId = attr.Value
+		}
+	}
+
+	exp, err := ParseExpressions(decoder, start, nil)
+	if err != nil {
+		return err
+	}
+
+	if len(exp) > 0 {
+		v.Expression = exp[0]
+	}
+
+	return nil
 }
 
 func (v VariableDefinition) Validate(errs *Errors) {
+	if v.VariableId == "" {
+		errs.Addf("VariableId not given")
+	}
+
+	if v.Expression == nil {
+		errs.Addf("Expression not given")
+	} else {
+		val := v.Expression.(Validatable)
+		val.Validate(errs.Sub("Expression"))
+	}
 }
--- type_VariableDefinition_test.go
+++ type_VariableDefinition_test.go
@@ -2,17 +2,28 @@ package xacml
 
 import (
 	"encoding/xml"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_VariableDefinition(t *testing.T) {
-	input := `<VariableDefinition></VariableDefinition>`
+	input := `
+	<VariableDefinition VariableId="the-variable">
+		<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">doctor</AttributeValue>
+	</VariableDefinition>`
 
 	dest := &VariableDefinition{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element VariableDefinition")
 
-	// Insert specific tests here
+	assert.Equal(t, "the-variable", dest.VariableId)
+
+	switch v := dest.Expression.(type) {
+	case *AttributeValue:
+		assert.Equal(t, "doctor", v.Value)
+	default:
+		panic(fmt.Sprintf("invalid Expression: %#v", v))
+	}
 }
