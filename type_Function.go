1fc9343204863e3201a14a9958db94ec951e29ff
Andrew Dunham <adunham@stripe.com>
2016-08-10 16:49:30 -0700

    Implement <Function>

--- type_Function.go
+++ type_Function.go
@@ -2,9 +2,11 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.28: The <Function> element SHALL be used to name a function as an argument
+// to the function defined by the parent <Apply> element.
 type Function struct {
 	XMLName xml.Name `xml:"Function"`
 
-	// Insert fields here
+	// The identifier of the function.
+	FunctionId string `xml:",attr"`
 }
--- type_Function_test.go
+++ type_Function_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func Test_Function(t *testing.T) {
-	input := `<Function></Function>`
+	input := `<Function FunctionId="urn:oasis:names:tc:xacml:1.0:function:string-equal"/>`
 
 	dest := &Function{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Function")
 
-	// Insert specific tests here
+	assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:function:string-equal", dest.FunctionId)
 }
