855d7be4c4cbc0e42974a2fff429f70948480e85
Andrew Dunham <adunham@stripe.com>
2016-08-09 18:46:43 -0700

    Implement <Attribute>

--- type_Attribute.go
+++ type_Attribute.go
@@ -2,12 +2,48 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.46: The <Attribute> element is the central abstraction of the request
+// context.  It contains attribute meta-data and one or more attribute values.
+// The attribute meta-data comprises the attribute identifier and the attribute
+// issuer.  <AttributeDesignator> elements in the policy MAY refer to
+// attributes by means of this meta-data.
 type Attribute struct {
 	XMLName xml.Name `xml:"Attribute"`
 
-	// Insert fields here
+	// The Attribute identifier.  A number of identifiers are reserved by
+	// XACML to denote commonly used attributes.  See Appendix B.
+	AttributeId string `xml:",attr"`
+
+	// The Attribute issuer.  For example, this attribute value MAY be an
+	// x500Name that binds to a public key, or it may be some other
+	// identifier exchanged out-of-band by issuing and relying parties.
+	Issuer *string `xml:",attr"`
+
+	// Whether to include this attribute in the result. This is useful to
+	// correlate requests with their responses in case of multiple
+	// requests.
+	// NOTE: Optional, defaults to false
+	IncludeInResult bool `xml:",attr"`
+
+	// One or more attribute values.  Each attribute value MAY have
+	// contents that are empty, occur once or occur multiple times.
+	AttributeValues []AttributeValue `xml:"AttributeValue"`
 }
 
 func (a Attribute) Validate(errs *Errors) {
+	if a.AttributeId == "" {
+		errs.Addf("AttributeId not given")
+	}
+
+	if a.Issuer != nil && *a.Issuer == "" {
+		errs.Addf("Issuer cannot be empty")
+	}
+
+	if len(a.AttributeValues) < 1 {
+		errs.Addf("AttributeValues not given")
+	} else {
+		for i, el := range a.AttributeValues {
+			el.Validate(errs.SubN("AttributeValues", i))
+		}
+	}
 }
--- type_Attribute_test.go
+++ type_Attribute_test.go
@@ -8,11 +8,36 @@ import (
 )
 
 func Test_Attribute(t *testing.T) {
-	input := `<Attribute></Attribute>`
+	input := `
+	<Attribute
+		IncludeInResult="true"
+		AttributeId="urn:oasis:names:tc:xacml:1.0:subject:subject-id">
+
+		<AttributeValue
+			DataType="http://www.w3.org/2001/XMLSchema#string">John Doe</AttributeValue>
+	</Attribute>`
 
 	dest := &Attribute{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Attribute")
 
-	// Insert specific tests here
+	assert.Equal(t, true, dest.IncludeInResult)
+	assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:subject:subject-id", dest.AttributeId)
+
+	if assert.Len(t, dest.AttributeValues, 1) {
+		assert.Equal(t, "John Doe", dest.AttributeValues[0].Value)
+	}
+}
+
+func TestAttributeNoValues(t *testing.T) {
+	input := `<Attribute AttributeId="fake"></Attribute>`
+
+	dest := &Attribute{}
+	err := xml.Unmarshal([]byte(input), dest)
+	assert.NoError(t, err, "Error unmarshalling input for element Attribute")
+
+	errs := NewErrors("Attribute")
+	dest.Validate(errs)
+	assert.Equal(t, 1, errs.NumErrors())
+	assert.Contains(t, errs.Summary(), "AttributeValues not given")
 }
