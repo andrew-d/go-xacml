d235726a64d465e335732196440994d06bcf2d8c
Andrew Dunham <adunham@stripe.com>
2016-08-10 21:13:59 -0700

    Implement <PolicyIdentifierList>

--- type_PolicyIdentifierList.go
+++ type_PolicyIdentifierList.go
@@ -2,9 +2,30 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.49: The <PolicyIdentifierList> element contains a list of policy and
+// policy set identifiers of policies which have been applicable to a request.
+// The list is unordered.
 type PolicyIdentifierList struct {
 	XMLName xml.Name `xml:"PolicyIdentifierList"`
 
-	// Insert fields here
+	// The identifier and version of a policy which was applicable to the
+	// request. See section 5.11. The <PolicyIdReference> element MUST use
+	// the Version attribute to specify the version and MUST NOT use the
+	// LatestVersion or EarliestVersion attributes.
+	PolicyIdReferences []PolicyIdReference `xml:"PolicyIdReference"`
+
+	// The identifier and version of a policy set which was applicable to
+	// the request. See section 5.10. The <PolicySetIdReference> element
+	// MUST use the Version attribute to specify the version and MUST NOT
+	// use the LatestVersion or EarliestVersion attributes.
+	PolicySetIdReferences []PolicySetIdReference `xml:"PolicySetIdReference"`
+}
+
+func (p PolicyIdentifierList) Validate(errs *Errors) {
+	for i, el := range p.PolicyIdReferences {
+		el.Validate(errs.SubN("PolicyIdReferences", i))
+	}
+	for i, el := range p.PolicySetIdReferences {
+		el.Validate(errs.SubN("PolicySetIdReferences", i))
+	}
 }
--- type_PolicyIdentifierList_test.go
+++ type_PolicyIdentifierList_test.go
@@ -8,11 +8,20 @@ import (
 )
 
 func Test_PolicyIdentifierList(t *testing.T) {
-	input := `<PolicyIdentifierList></PolicyIdentifierList>`
+	input := `
+	<PolicyIdentifierList>
+		<PolicyIdReference>policy1</PolicyIdReference>
+		<PolicyIdReference>policy2</PolicyIdReference>
+		<PolicySetIdReference>policyset1</PolicySetIdReference>
+		<PolicySetIdReference>policyset2</PolicySetIdReference>
+	</PolicyIdentifierList>`
 
 	dest := &PolicyIdentifierList{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element PolicyIdentifierList")
 
-	// Insert specific tests here
+	assert.Equal(t, "policy1", dest.PolicyIdReferences[0].Value)
+	assert.Equal(t, "policy2", dest.PolicyIdReferences[1].Value)
+	assert.Equal(t, "policyset1", dest.PolicySetIdReferences[0].Value)
+	assert.Equal(t, "policyset2", dest.PolicySetIdReferences[1].Value)
 }
