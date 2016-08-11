5dec94dd77a31c64f10fc12f28e8ed14cbc23fc7
Andrew Dunham <adunham@stripe.com>
2016-08-10 20:13:48 -0700

    Implement <CombinerParameter>

--- type_CombinerParameter.go
+++ type_CombinerParameter.go
@@ -2,9 +2,29 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.17: The <CombinerParameter> element conveys a single parameter for a
+// policy- or rule-combining algorithm.  Support for the <CombinerParameter>
+// element is optional.
 type CombinerParameter struct {
 	XMLName xml.Name `xml:"CombinerParameter"`
 
-	// Insert fields here
+	// The identifier of the parameter.
+	ParameterName string `xml:",attr"`
+
+	// The value of the parameter.
+	AttributeValues []AttributeValue `xml:"AttributeValue"`
+}
+
+func (c CombinerParameter) Validate(errs *Errors) {
+	if c.ParameterName == "" {
+		errs.Addf("ParameterName not given")
+	}
+
+	if len(c.AttributeValues) < 1 {
+		errs.Addf("AttributeValues not given")
+	} else {
+		for i, el := range c.AttributeValues {
+			el.Validate(errs.SubN("AttributeValues", i))
+		}
+	}
 }
--- type_CombinerParameter_test.go
+++ type_CombinerParameter_test.go
@@ -8,11 +8,20 @@ import (
 )
 
 func Test_CombinerParameter(t *testing.T) {
-	input := `<CombinerParameter></CombinerParameter>`
+	input := `
+	<CombinerParameter ParameterName="input1">
+		<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeValue>
+		<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value2</AttributeValue>
+	</CombinerParameter>`
 
 	dest := &CombinerParameter{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element CombinerParameter")
 
-	// Insert specific tests here
+	assert.Equal(t, "input1", dest.ParameterName)
+
+	if assert.Len(t, dest.AttributeValues, 2) {
+		assert.Equal(t, "value1", dest.AttributeValues[0].Value)
+		assert.Equal(t, "value2", dest.AttributeValues[1].Value)
+	}
 }
