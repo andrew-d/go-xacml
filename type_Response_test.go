15ce9295e1f75eee390fea04be6c51ac90877756
Andrew Dunham <adunham@stripe.com>
2016-08-11 16:39:38 -0700

    Add tests for <Response>

--- type_Response_test.go
+++ type_Response_test.go
@@ -8,12 +8,28 @@ import (
 )
 
 func Test_Response(t *testing.T) {
-	input := `<Response></Response>`
+	input := `
+	<Response>
+		<Result>
+			<Decision>Permit</Decision>
+			<Status>
+				<StatusCode Value="urn:oasis:names:tc:xacml:1.0:status:ok"/>
+			</Status>
+		</Result>
+		<Result>
+			<Decision>Deny</Decision>
+			<Status>
+				<StatusCode Value="urn:oasis:names:tc:xacml:1.0:status:ok"/>
+			</Status>
+		</Result>
+	</Response>`
 
 	dest := &Response{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Response")
 
-	// Insert specific tests here
-	// TODO: add me
+	if assert.Len(t, dest.Results, 2) {
+		assert.Equal(t, "Permit", dest.Results[0].Decision.Value)
+		assert.Equal(t, "Deny", dest.Results[1].Decision.Value)
+	}
 }
