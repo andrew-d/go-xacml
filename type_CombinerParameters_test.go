830817d291d9e0234d6aae1d95226161f9c13c61
Andrew Dunham <adunham@stripe.com>
2016-08-10 20:59:31 -0700

    Add tests for <CombinerParameters>

--- type_CombinerParameters_test.go
+++ type_CombinerParameters_test.go
@@ -8,11 +8,18 @@ import (
 )
 
 func Test_CombinerParameters(t *testing.T) {
-	input := `<CombinerParameters></CombinerParameters>`
+	input := `
+	<CombinerParameters PolicySetIdRef="policyset1">
+		<CombinerParameter ParameterName="input1">
+			<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeValue>
+		</CombinerParameter>
+	</CombinerParameters>`
 
 	dest := &CombinerParameters{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element CombinerParameters")
 
-	// Insert specific tests here
+	if assert.Len(t, dest.CombinerParameters, 1) && assert.Len(t, dest.CombinerParameters[0].AttributeValues, 1) {
+		assert.Equal(t, "value1", dest.CombinerParameters[0].AttributeValues[0].Value)
+	}
 }
