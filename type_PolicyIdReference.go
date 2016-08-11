56d1192db877aba403fc8e0a52c82c199f245f07
Andrew Dunham <adunham@stripe.com>
2016-08-10 20:38:47 -0700

    Implement <PolicyIdReference>

--- type_PolicyIdReference.go
+++ type_PolicyIdReference.go
@@ -2,12 +2,32 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.11: The <PolicyIdReference> element SHALL be used to reference a <Policy>
+// element by id.  If <PolicyIdReference> is a URL, then it MAY be resolvable
+// to the <Policy> element.  However, the mechanism for resolving a policy
+// reference to the corresponding policy is outside the scope of this
+// specification.
 type PolicyIdReference struct {
 	XMLName xml.Name `xml:"PolicyIdReference"`
 
-	// Insert fields here
+	// Specifies a matching expression for the version of the policy
+	// referenced.
+	Version string `xml:",attr"`
+
+	// Specifies a matching expression for the earliest acceptable version
+	// of the policy referenced.
+	EarliestVersion string `xml:",attr"`
+
+	// Specifies a matching expression for the latest acceptable version of
+	// the policy referenced.
+	LatestVersion string `xml:",attr"`
+
+	// TODO: Do we need to use "innerxml", or can we actually use "chardata"?
+	Value string `xml:",chardata"`
 }
 
 func (p PolicyIdReference) Validate(errs *Errors) {
+	if p.Value == "" {
+		errs.Addf("Value not given")
+	}
 }
--- type_PolicyIdReference_test.go
+++ type_PolicyIdReference_test.go
@@ -8,11 +8,20 @@ import (
 )
 
 func Test_PolicyIdReference(t *testing.T) {
-	input := `<PolicyIdReference></PolicyIdReference>`
+	input := `
+	<PolicyIdReference
+		Version="1.0"
+		EarliestVersion="0.1"
+		LatestVersion="2.0"
+		>urn:oasis:names:tc:xacml:2.0:conformance-test:IIE001:policy1</PolicyIdReference>`
 
 	dest := &PolicyIdReference{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element PolicyIdReference")
 
-	// Insert specific tests here
+	assert.Equal(t, "1.0", dest.Version)
+	assert.Equal(t, "0.1", dest.EarliestVersion)
+	assert.Equal(t, "2.0", dest.LatestVersion)
+
+	assert.Equal(t, "urn:oasis:names:tc:xacml:2.0:conformance-test:IIE001:policy1", dest.Value)
 }
