4165b7905d6e153026ef8794f9fbf8aa756ba1ae
Andrew Dunham <adunham@stripe.com>
2016-08-11 12:14:39 -0700

    Implement <Obligation>

--- type_Obligation.go
+++ type_Obligation.go
@@ -2,9 +2,27 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.34: The <Obligation> element SHALL contain an identifier for the
+// obligation and a set of attributes that form arguments of the action defined
+// by the obligation.
 type Obligation struct {
 	XMLName xml.Name `xml:"Obligation"`
 
-	// Insert fields here
+	// Obligation identifier.  The value of the obligation identifier SHALL
+	// be interpreted by the PEP.
+	ObligationId string `xml:",attr"`
+
+	// Obligation arguments assignment.  The values of the obligation
+	// arguments SHALL be interpreted by the PEP.
+	Assignments []AttributeAssignment `xml:"AttributeAssignment"`
+}
+
+func (o Obligation) Validate(errs *Errors) {
+	if o.ObligationId == "" {
+		errs.Addf("ObligationId not given")
+	}
+
+	for i, el := range o.Assignments {
+		el.Validate(errs.SubN("Assignments", i))
+	}
 }
--- type_Obligation_test.go
+++ type_Obligation_test.go
@@ -8,11 +8,22 @@ import (
 )
 
 func Test_Obligation(t *testing.T) {
-	input := `<Obligation></Obligation>`
+	input := `
+	<Obligation
+		ObligationId="obligation-id" >
+
+		<AttributeAssignment
+			AttributeId="attribute-1"
+			DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeAssignment>
+	</Obligation>`
 
 	dest := &Obligation{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Obligation")
 
-	// Insert specific tests here
+	assert.Equal(t, "obligation-id", dest.ObligationId)
+	if assert.Len(t, dest.Assignments, 1) {
+		assert.Equal(t, "attribute-1", dest.Assignments[0].AttributeId)
+		assert.Equal(t, "value1", dest.Assignments[0].Value)
+	}
 }
