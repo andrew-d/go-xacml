8b7a36e76ea6c544763bd59d2954a7a560181c3c
Andrew Dunham <adunham@stripe.com>
2016-08-11 16:25:38 -0700

    Finish implementing <Result>

--- type_Result.go
+++ type_Result.go
@@ -10,9 +10,64 @@ import "encoding/xml"
 type Result struct {
 	XMLName xml.Name `xml:"Result"`
 
-	// TODO: finish me
+	// The authorization decision: “Permit”, “Deny”, “Indeterminate” or
+	// “NotApplicable”.
+	Decision Decision `xml:"Decision"`
+
+	// Indicates whether errors occurred during evaluation of the decision
+	// request, and optionally, information about those errors.  If the
+	// <Response> element contains <Result> elements whose <Status>
+	// elements are all identical, and the <Response> element is contained
+	// in a protocol wrapper that can convey status information, then the
+	// common status information MAY be placed in the protocol wrapper and
+	// this <Status> element MAY be omitted from all <Result> elements.
+	Status *Status `xml:"Status"`
+
+	// A list of obligations that MUST be fulfilled by the PEP.  If the PEP
+	// does not understand or cannot fulfill an obligation, then the action
+	// of the PEP is determined by its bias, see section 7.2.  See Section
+	// 7.18 for a description of how the set of obligations to be returned
+	// by the PDP is determined.
+	Obligations *Obligations `xml:"Obligations"`
+
+	// A list of advice that provide supplemental information to the PEP.
+	// If the PEP does not understand an advice, the PEP may safely ignore
+	// the advice. See Section 7.18 for a description of how the set of
+	// advice to be returned by the PDP is determined.
+	AssociatedAdvice *AssociatedAdvice `xml:"AssociatedAdvice"`
+
+	// A list of attributes that were part of the request. The choice of
+	// which attributes are included here is made with the IncludeInResult
+	// attribute of the <Attribute> elements of the request. See section
+	// 5.46.
+	Attributes []Attributes `xml:"Attributes"`
+
+	// If the ReturnPolicyIdList attribute in the <Request> is true (see
+	// section 5.42), a PDP that implements this optional feature MUST
+	// return a list of all policies which were found to be fully
+	// applicable. That is, all policies where both the <Target> matched
+	// and the <Condition> evaluated to true, whether or not the <Effect>
+	// was the same or different from the <Decision>.
+	PolicyIdentifierList *PolicyIdentifierList `xml:"PolicyIdentifierList"`
 }
 
 func (r Result) Validate(errs *Errors) {
-	// TODO: finish me
+	r.Decision.Validate(errs.Sub("Decision"))
+
+	if r.Status != nil {
+		r.Status.Validate(errs.Sub("Status"))
+	}
+	if r.Obligations != nil {
+		r.Obligations.Validate(errs.Sub("Obligations"))
+	}
+	if r.AssociatedAdvice != nil {
+		r.AssociatedAdvice.Validate(errs.Sub("AssociatedAdvice"))
+	}
+	if r.PolicyIdentifierList != nil {
+		r.PolicyIdentifierList.Validate(errs.Sub("PolicyIdentifierList"))
+	}
+
+	for i, el := range r.Attributes {
+		el.Validate(errs.SubN("Attributes", i))
+	}
 }
--- type_Result_test.go
+++ type_Result_test.go
@@ -8,11 +8,77 @@ import (
 )
 
 func Test_Result(t *testing.T) {
-	input := `<Result></Result>`
+	input := `
+	<Result>
+		<Decision>Deny</Decision>
+		<Status>
+			<StatusCode Value="urn:oasis:names:tc:xacml:1.0:status:ok"/>
+		</Status>
+
+		<Obligations>
+			<Obligation ObligationId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:obligation-1">
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:assignment1"
+					DataType="http://www.w3.org/2001/XMLSchema#string">assignment1</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicSingleValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">J. Hibbert</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicMultiValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">C. Everet Koop</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicMultiValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">Victor Frankenstein</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicMultiValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">John Jeckel</AttributeAssignment>
+			</Obligation>
+		</Obligations>
+		<AssociatedAdvice>
+			<Advice AdviceId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:Advice-1">
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:assignment1"
+					DataType="http://www.w3.org/2001/XMLSchema#string">assignment1</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicSingleValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">J. Hibbert</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicMultiValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">C. Everet Koop</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicMultiValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">Victor Frankenstein</AttributeAssignment>
+				<AttributeAssignment
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:IID302:dynamicMultiValue"
+					DataType="http://www.w3.org/2001/XMLSchema#string">John Jeckel</AttributeAssignment>
+			</Advice>
+		</AssociatedAdvice>
+		<PolicyIdentifierList>
+			<PolicyIdReference Version="1.0">policy1</PolicyIdReference>
+			<PolicySetIdReference Version="1.0">policyset1</PolicySetIdReference>
+		</PolicyIdentifierList>
+		<Attributes Category="attributes-category">
+			<Attribute IncludeInResult="true" AttributeId="attribute1">
+				<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeValue>
+			</Attribute>
+		</Attributes>
+	</Result>`
 
 	dest := &Result{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Result")
 
-	// Insert specific tests here
+	errs := NewErrors("Result")
+	dest.Validate(errs)
+	assert.Equal(t, 0, errs.NumErrors(), "Validate() returned errors:\n%s", errs.Summary())
+
+	assert.Equal(t, "Deny", dest.Decision.Value)
+	assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:status:ok", dest.Status.StatusCode.Value)
+	assert.Len(t, dest.Obligations.Obligations, 1)
+	assert.Len(t, dest.AssociatedAdvice.Advice, 1)
+	assert.Len(t, dest.PolicyIdentifierList.PolicyIdReferences, 1)
+	assert.Len(t, dest.PolicyIdentifierList.PolicySetIdReferences, 1)
+	if assert.Len(t, dest.Attributes, 1) {
+		assert.Len(t, dest.Attributes[0].Attributes, 1)
+	}
 }
