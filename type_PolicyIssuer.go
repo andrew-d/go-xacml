895f6f0ef34bc31b37a0fd25b217b66053d8d60d
Andrew Dunham <adunham@stripe.com>
2016-08-10 20:02:46 -0700

    Implement <PolicyIssuer>

--- type_PolicyIssuer.go
+++ type_PolicyIssuer.go
@@ -2,12 +2,27 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.3: The <PolicyIssuer> element contains attributes describing the issuer of
+// the policy or policy set. The use of the policy issuer element is defined in
+// a separate administration profile [XACMLAdmin]. A PDP which does not
+// implement the administration profile MUST report an error or return an
+// Indeterminate result if it encounters this element.
 type PolicyIssuer struct {
 	XMLName xml.Name `xml:"PolicyIssuer"`
 
-	// Insert fields here
+	// Free form XML describing the issuer. See Section 5.45.
+	Content *Content `xml:"Content"`
+
+	// An attribute of the issuer. See Section 5.46.
+	Attributes []Attribute `xml:"Attribute"`
 }
 
 func (p PolicyIssuer) Validate(errs *Errors) {
+	if p.Content != nil {
+		p.Content.Validate(errs.Sub("Content"))
+	}
+
+	for i, el := range p.Attributes {
+		el.Validate(errs.SubN("Attributes", i))
+	}
 }
--- type_PolicyIssuer_test.go
+++ type_PolicyIssuer_test.go
@@ -8,11 +8,25 @@ import (
 )
 
 func Test_PolicyIssuer(t *testing.T) {
-	input := `<PolicyIssuer></PolicyIssuer>`
+	input := `
+	<PolicyIssuer>
+		<Content>foo</Content>
+		<Attribute
+			IncludeInResult="true"
+			AttributeId="urn:oasis:names:tc:xacml:1.0:subject:subject-id">
+
+			<AttributeValue
+				DataType="http://www.w3.org/2001/XMLSchema#string">val1</AttributeValue>
+		</Attribute>
+	</PolicyIssuer>
+	`
 
 	dest := &PolicyIssuer{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element PolicyIssuer")
 
-	// Insert specific tests here
+	assert.Equal(t, "foo", dest.Content.XML)
+	if assert.Len(t, dest.Attributes, 1) {
+		assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:subject:subject-id", dest.Attributes[0].AttributeId)
+	}
 }
