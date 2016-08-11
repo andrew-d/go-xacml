ba2cbd40baac08c0da557af6e8addd4833455a61
Andrew Dunham <adunham@stripe.com>
2016-08-11 16:13:10 -0700

    Implement <MissingAttributeDetail>

--- type_MissingAttributeDetail.go
+++ type_MissingAttributeDetail.go
@@ -2,9 +2,42 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.58: The <MissingAttributeDetail> element conveys information about
+// attributes required for policy evaluation that were missing from the request
+// context.
 type MissingAttributeDetail struct {
 	XMLName xml.Name `xml:"MissingAttributeDetail"`
 
-	// Insert fields here
+	// The category identifier of the missing attribute.
+	Category string `xml:",attr"`
+
+	// The identifier of the missing attribute.
+	AttributeId string `xml:",attr"`
+
+	// The data-type of the missing attribute.
+	DataType string `xml:",attr"`
+
+	// This attribute, if supplied, SHALL specify the required Issuer of
+	// the missing attribute.
+	Issuer *string `xml:",attr"`
+
+	// The required value of the missing attribute.
+	AttributeValue AttributeValue
+}
+
+func (m MissingAttributeDetail) Validate(errs *Errors) {
+	if m.Category == "" {
+		errs.Addf("Category not given")
+	}
+	if m.AttributeId == "" {
+		errs.Addf("AttributeId not given")
+	}
+	if m.DataType == "" {
+		errs.Addf("DataType not given")
+	}
+	if m.Issuer != nil && *m.Issuer == "" {
+		errs.Addf("Issuer given, but cannot be empty")
+	}
+
+	m.AttributeValue.Validate(errs.Sub("AttributeValue"))
 }
--- type_MissingAttributeDetail_test.go
+++ type_MissingAttributeDetail_test.go
@@ -8,11 +8,24 @@ import (
 )
 
 func Test_MissingAttributeDetail(t *testing.T) {
-	input := `<MissingAttributeDetail></MissingAttributeDetail>`
+	input := `
+	<MissingAttributeDetail
+		Category="category1"
+		AttributeId="attribute1"
+		DataType="http://www.w3.org/2001/XMLSchema#string"
+		Issuer="issuer1">
+
+		<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeValue>
+	</MissingAttributeDetail>`
 
 	dest := &MissingAttributeDetail{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element MissingAttributeDetail")
 
-	// Insert specific tests here
+	assert.Equal(t, "category1", dest.Category)
+	assert.Equal(t, "attribute1", dest.AttributeId)
+	assert.Equal(t, "http://www.w3.org/2001/XMLSchema#string", dest.DataType)
+	assert.Equal(t, "issuer1", *dest.Issuer)
+
+	assert.Equal(t, "value1", dest.AttributeValue.Value)
 }
