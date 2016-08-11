aeb38e9a3c7639519da4e829340a3502cea0626e
Andrew Dunham <adunham@stripe.com>
2016-08-11 16:00:39 -0700

    Implement <PolicyCombinerParameters>

--- type_PolicyCombinerParameters.go
+++ type_PolicyCombinerParameters.go
@@ -2,12 +2,41 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.19: The <PolicyCombinerParameters> element conveys parameters associated
+// with a particular policy within a policy set for a policy-combining
+// algorithm.
+//
+// Each <PolicyCombinerParameters> element MUST be associated with a policy
+// contained within the same policy set.  If multiple
+// <PolicyCombinerParameters> elements reference the same policy, they SHALL be
+// considered equal to one <PolicyCombinerParameters> element containing the
+// concatenation of all the sequences of <CombinerParameters> contained in all
+// the aforementioned <PolicyCombinerParameters> elements, such that the order
+// of occurrence of the <PolicyCominberParameters> elements is preserved in the
+// concatenation of the <CombinerParameter> elements.
+//
+// Note that none of the policy-combining algorithms specified in XACML 3.0 is
+// parameterized.
+//
+// Support for the <PolicyCombinerParameters> element is optional, only if
+// support for combiner parameters is not implemented.
 type PolicyCombinerParameters struct {
 	XMLName xml.Name `xml:"PolicyCombinerParameters"`
 
-	// Insert fields here
+	// The identifier of a <Policy> or the value of a <PolicyIdReference>
+	// contained in the policy set.
+	PolicyIdRef string `xml:",attr"`
+
+	// A single parameter.  See Section 5.17.
+	CombinerParameters []CombinerParameter `xml:"CombinerParameter"`
 }
 
 func (p PolicyCombinerParameters) Validate(errs *Errors) {
+	if p.PolicyIdRef == "" {
+		errs.Addf("PolicyIdRef not given")
+	}
+
+	for i, el := range p.CombinerParameters {
+		el.Validate(errs.SubN("CombinerParameters", i))
+	}
 }
--- type_PolicyCombinerParameters_test.go
+++ type_PolicyCombinerParameters_test.go
@@ -8,11 +8,19 @@ import (
 )
 
 func Test_PolicyCombinerParameters(t *testing.T) {
-	input := `<PolicyCombinerParameters></PolicyCombinerParameters>`
+	input := `
+	<PolicyCombinerParameters PolicyIdRef="policyset1">
+		<CombinerParameter ParameterName="input1">
+			<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeValue>
+		</CombinerParameter>
+	</PolicyCombinerParameters>`
 
 	dest := &PolicyCombinerParameters{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element PolicyCombinerParameters")
 
-	// Insert specific tests here
+	assert.Equal(t, "policyset1", dest.PolicyIdRef)
+	if assert.Len(t, dest.CombinerParameters, 1) && assert.Len(t, dest.CombinerParameters[0].AttributeValues, 1) {
+		assert.Equal(t, "value1", dest.CombinerParameters[0].AttributeValues[0].Value)
+	}
 }
