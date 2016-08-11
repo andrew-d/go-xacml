df6c682458368ccead8cfdf28b84bdb4362c2745
Andrew Dunham <adunham@stripe.com>
2016-08-11 16:08:14 -0700

    Implement <RuleCombinerParameters>

--- type_RuleCombinerParameters.go
+++ type_RuleCombinerParameters.go
@@ -2,12 +2,39 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.18: The <RuleCombinerParameters> element conveys parameters associated
+// with a particular rule within a policy for a rule-combining algorithm.
+//
+// Each <RuleCombinerParameters> element MUST be associated with a rule
+// contained within the same policy.  If multiple <RuleCombinerParameters>
+// elements reference the same rule, they SHALL be considered equal to one
+// <RuleCombinerParameters> element containing the concatenation of all the
+// sequences of <CombinerParameters> contained in all the aforementioned
+// <RuleCombinerParameters> elements, such that the order of occurrence of the
+// <RuleCominberParameters> elements is preserved in the concatenation of the
+// <CombinerParameter> elements.
+//
+// Note that none of the rule-combining algorithms specified in XACML 3.0 is
+// parameterized.
+//
+// Support for the <RuleCombinerParameters> element is optional, only if
+// support for combiner parameters is not implemented.
 type RuleCombinerParameters struct {
 	XMLName xml.Name `xml:"RuleCombinerParameters"`
 
-	// Insert fields here
+	// The identifier of the <Rule> contained in the policy.
+	RuleIdRef string `xml:",attr"`
+
+	// A single parameter.  See Section 5.17.
+	CombinerParameters []CombinerParameter `xml:"CombinerParameter"`
 }
 
 func (r RuleCombinerParameters) Validate(errs *Errors) {
+	if r.RuleIdRef == "" {
+		errs.Addf("RuleIdRef not given")
+	}
+
+	for i, el := range r.CombinerParameters {
+		el.Validate(errs.SubN("CombinerParameters", i))
+	}
 }
--- type_RuleCombinerParameters_test.go
+++ type_RuleCombinerParameters_test.go
@@ -8,11 +8,19 @@ import (
 )
 
 func Test_RuleCombinerParameters(t *testing.T) {
-	input := `<RuleCombinerParameters></RuleCombinerParameters>`
+	input := `
+	<RuleCombinerParameters RuleIdRef="rule1">
+		<CombinerParameter ParameterName="input1">
+			<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeValue>
+		</CombinerParameter>
+	</RuleCombinerParameters>`
 
 	dest := &RuleCombinerParameters{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element RuleCombinerParameters")
 
-	// Insert specific tests here
+	assert.Equal(t, "rule1", dest.RuleIdRef)
+	if assert.Len(t, dest.CombinerParameters, 1) && assert.Len(t, dest.CombinerParameters[0].AttributeValues, 1) {
+		assert.Equal(t, "value1", dest.CombinerParameters[0].AttributeValues[0].Value)
+	}
 }
