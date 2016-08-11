870654a9ffde553265e036d89afade792a5cd033
Andrew Dunham <adunham@stripe.com>
2016-08-10 19:40:49 -0700

    Implement <Condition>

--- type_Condition.go
+++ type_Condition.go
@@ -2,12 +2,36 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.26: The <Condition> element is a Boolean function over attributes or
+// functions of attributes.
 type Condition struct {
 	XMLName xml.Name `xml:"Condition"`
 
-	// Insert fields here
+	// The <Condition> contains one <Expression> element, with the
+	// restriction that the <Expression> return data-type MUST be
+	// “http://www.w3.org/2001/XMLSchema#boolean”.  Evaluation of the
+	// <Condition> element is described in Section 7.9.
+	Expression ExpressionType
+}
+
+func (c *Condition) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
+	exp, err := ParseExpressions(decoder, start, nil)
+	if err != nil {
+		return err
+	}
+
+	if len(exp) > 0 {
+		c.Expression = exp[0]
+	}
+
+	return nil
 }
 
 func (c Condition) Validate(errs *Errors) {
+	if c.Expression == nil {
+		errs.Addf("Expression not given")
+	} else {
+		val := c.Expression.(Validatable)
+		val.Validate(errs.Sub("Expression"))
+	}
 }
--- type_Condition_test.go
+++ type_Condition_test.go
@@ -8,11 +8,26 @@ import (
 )
 
 func Test_Condition(t *testing.T) {
-	input := `<Condition></Condition>`
+	input := `
+	<Condition>
+		<Apply FunctionId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+			<Apply FunctionId="urn:oasis:names:tc:xacml:1.0:function:string-one-and-only">
+				<AttributeDesignator
+					AttributeId="urn:oasis:names:tc:xacml:2.0:conformance-test:test"
+					Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject"
+					DataType="http://www.w3.org/2001/XMLSchema#string"
+					MustBePresent="false"/>
+			</Apply>
+			<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">John Doe</AttributeValue>
+		</Apply>
+	</Condition>`
 
 	dest := &Condition{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element Condition")
 
-	// Insert specific tests here
+	if assert.NotNil(t, dest.Expression) {
+		cond := dest.Expression.(*Apply)
+		assert.Equal(t, "urn:oasis:names:tc:xacml:1.0:function:string-equal", cond.FunctionId)
+	}
 }
