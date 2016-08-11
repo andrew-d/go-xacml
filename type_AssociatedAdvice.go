6fb057f3fe035df1987b0255829ce3cae9c93230
Andrew Dunham <adunham@stripe.com>
2016-08-11 15:57:21 -0700

    Implement <AssociatedAdvice>

--- type_AssociatedAdvice.go
+++ type_AssociatedAdvice.go
@@ -2,9 +2,21 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.33: The <AssociatedAdvice> element SHALL contain a set of <Advice>
+// elements.
 type AssociatedAdvice struct {
 	XMLName xml.Name `xml:"AssociatedAdvice"`
 
-	// Insert fields here
+	// A sequence of advice.  See Section 5.35.
+	Advice []Advice `xml:"Advice"`
+}
+
+func (a AssociatedAdvice) Validate(errs *Errors) {
+	if len(a.Advice) < 1 {
+		errs.Addf("Advice not given")
+	} else {
+		for i, el := range a.Advice {
+			el.Validate(errs.SubN("Advice", i))
+		}
+	}
 }
--- type_AssociatedAdvice_test.go
+++ type_AssociatedAdvice_test.go
@@ -8,11 +8,20 @@ import (
 )
 
 func Test_AssociatedAdvice(t *testing.T) {
-	input := `<AssociatedAdvice></AssociatedAdvice>`
+	input := `
+	<AssociatedAdvice>
+		<Advice AdviceId="advice-1">
+			<AttributeAssignment
+				AttributeId="attribute-1"
+				DataType="http://www.w3.org/2001/XMLSchema#string">value-1</AttributeAssignment>
+		</Advice>
+	</AssociatedAdvice>`
 
 	dest := &AssociatedAdvice{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element AssociatedAdvice")
 
-	// Insert specific tests here
+	if assert.Len(t, dest.Advice, 1) {
+		assert.Equal(t, "advice-1", dest.Advice[0].AdviceId)
+	}
 }
