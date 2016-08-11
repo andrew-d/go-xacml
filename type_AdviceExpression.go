35a7bf294119498ba24ff79b8320a59a4880c057
Andrew Dunham <adunham@stripe.com>
2016-08-11 15:43:59 -0700

    Implement <AdviceExpression>

--- type_AdviceExpression.go
+++ type_AdviceExpression.go
@@ -2,9 +2,46 @@ package xacml
 
 import "encoding/xml"
 
-// 5.XX: TODO DESCRIPTION HERE
+// 5.40: The <AdviceExpression> element evaluates to an advice and SHALL
+// contain an identifier for an advice and a set of expressions that form
+// arguments of the supplemental information defined by the advice.  The
+// AppliesTo attribute SHALL indicate the effect for which this advice must be
+// provided to the PEP.
 type AdviceExpression struct {
 	XMLName xml.Name `xml:"AdviceExpression"`
 
-	// Insert fields here
+	// Advice identifier.  The value of the advice identifier MAY be
+	// interpreted by the PEP.
+	AdviceId string `xml:",attr"`
+
+	// The effect for which this advice must be provided to the PEP.
+	AppliesTo string `xml:",attr"`
+
+	// Advice arguments in the form of expressions. The expressions SHALL
+	// be evaluated by the PDP to constant <AttributeValue> elements or
+	// bags, which shall be the attribute assignments in the <Advice>
+	// returned to the PEP.  If an <AttributeAssignmentExpression>
+	// evaluates to an atomic attribute value, then there MUST be one
+	// resulting <AttributeAssignment> which MUST contain this single
+	// attribute value. If the <AttributeAssignmentExpression> evaluates to
+	// a bag, then there MUST be a resulting <AttributeAssignment> for each
+	// of the values in the bag. If the bag is empty, there shall be no
+	// <AttributeAssignment> from this <AttributeAssignmentExpression>.
+	// The values of the advice arguments MAY be interpreted by the PEP.
+	AttributeAssignmentExpressions []AttributeAssignmentExpression `xml:"AttributeAssignmentExpression"`
+}
+
+func (a AdviceExpression) Validate(errs *Errors) {
+	if a.AdviceId == "" {
+		errs.Addf("AdviceId not given")
+	}
+	if a.AppliesTo == "" {
+		errs.Addf("AppliesTo not given")
+	} else if a.AppliesTo != "Permit" && a.AppliesTo != "Deny" {
+		errs.Addf("Invalid value for AppliesTo: %q", a.AppliesTo)
+	}
+
+	for i, el := range a.AttributeAssignmentExpressions {
+		el.Validate(errs.SubN("AttributeAssignmentExpressions", i))
+	}
 }
--- type_AdviceExpression_test.go
+++ type_AdviceExpression_test.go
@@ -8,11 +8,26 @@ import (
 )
 
 func Test_AdviceExpression(t *testing.T) {
-	input := `<AdviceExpression></AdviceExpression>`
+	input := `
+	<AdviceExpression AppliesTo="Deny" AdviceId="advice1">
+		<AttributeAssignmentExpression AttributeId="attribute1">
+			<AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">value1</AttributeValue>
+		</AttributeAssignmentExpression>
+	</AdviceExpression>
+	`
 
 	dest := &AdviceExpression{}
 	err := xml.Unmarshal([]byte(input), dest)
 	assert.NoError(t, err, "Error unmarshalling input for element AdviceExpression")
 
-	// Insert specific tests here
+	assert.Equal(t, "advice1", dest.AdviceId)
+	assert.Equal(t, "Deny", dest.AppliesTo)
+
+	if assert.Len(t, dest.AttributeAssignmentExpressions, 1) {
+		aae := dest.AttributeAssignmentExpressions[0]
+		assert.Equal(t, "attribute1", aae.AttributeId)
+
+		val := aae.Expression.(*AttributeValue)
+		assert.Equal(t, "value1", val.Value)
+	}
 }
